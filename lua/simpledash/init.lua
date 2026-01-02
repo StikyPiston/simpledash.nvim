6aa44f1cdca58ba10f15253ec812305612ab6313
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-02 21:56:51 +0200

    feat: Make the dashboard show on setup

--- lua/simpledash/init.lua
+++ lua/simpledash/init.lua
@@ -1,5 +1,23 @@
+local dashboard = require("simpledash.dashboard")
+
 local M = {}
 
+function M.setup()
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      -- If files were passed as args, do nothing
+      if vim.fn.argc() > 0 then
+        return
+      end
 
+      -- If already in a real buffer, do nothing
+      if vim.bo.filetype ~= "" then
+        return
+      end
+
+      dashboard.show()
+    end,
+  })
+end
 
 return M
